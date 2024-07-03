e96373481fdc7309ec0499c64281b58acab0be3c
Khushboo Bhagchandani <119107041+Khushboo2525@users.noreply.github.com>
2024-07-03 20:07:19 +0530

    final

--- Mess.java
+++ Mess.java
@@ -1,9 +1,7 @@
 public class Mess{
 public static void main(String[] args) {
     int year = 2001;
-    String title = "Shrek";
     System.out.println(year);
-    System.out.println(title);
 
 }
 }
